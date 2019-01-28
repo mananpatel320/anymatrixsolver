3dc4f70e8aba8e2d16d5ee456c04a5256d9e3d5a
ManaN Patel <mananpatel320@gmail.com>
2019-01-28 13:03:10 +0530

    significant improvements

--- anymatrix.cpp
+++ anymatrix.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    float A[100][100],t,x,B[100];
-    int R,C,k=0,m=0,l=0;
+    float A[100][100],t,x,B[100],z;
+    int R,C,k=0,m=0,l=0,n=0,p=0;
     cout<<"Rows:";cin>>R;        //input rows in augmented matrix
     cout<<"Columns:";cin>>C;     //input columns in augmented matrix
     for(int i=0;i<R;i++)         //augmented matrix input loop
@@ -24,7 +24,7 @@ int main()
         cout<<endl;
     }
     while(k<R)
-    {if(A[k][k]==0)            //Row interchange if element of primary diagonal of coeffecient matrix is zero
+    {if(A[k][k]==0)            //Row interchange if element of primary diagonal of coefficient matrix is zero
     {
         for(int i=k+1;i<R;++i)
         {
@@ -36,11 +36,52 @@ int main()
                     A[i][j]=A[k][j];
                     A[k][j]=t;
                 }
+                ++n;
                 break;
             }
         }
+      for(int j=0;j<C;j++)
+    {
+        if(A[k][j]!=0)
+            ++l;
     }
-    /*for(int i=0;i<R;i++)
+    if(l==0)             //If all the elements of a row are 0 then there are infinite solutions
+    {
+        cout<<"\nThere are infinitely many solutions of the system of equations.\n";
+        return 0;
+    }
+    else if(l==1&&A[k][C-1]!=0)  //If all the elements, except the last one, of a row are 0 then no solution exists
+    {
+        cout<<"\nNo solution exits for the system system of equations.\n";
+        return 0;
+    }
+    l=0;
+      z=A[k][C-1]/A[k+1][C-1];
+      for(int j=k+1;j<C;j++)
+
+      {
+          if((A[k][j]/A[k+1][j])!=z)
+            ++p;
+      }
+     if(n==0&&A[k][C-1]!=0)
+     {
+        if(p!=0)
+        {
+         cout<<"\nNo solution exists for the system of equations.\n";
+         return 0;
+        }
+        else
+        {
+            cout<<"\nThe system of equations has infinite many solutions.\n";
+         return 0;
+        }
+     }
+
+     n=0;
+     p=0;
+    }
+
+    for(int i=0;i<R;i++)
     {
         for(int j=0;j<C;j++)
         {
@@ -49,18 +90,8 @@ int main()
         cout<<endl;
     }
     cout<<endl;
-    */
-    for(int j=0;j<C;j++)
-    {
-        if(A[k][j]!=0)
-            ++l;
-    }
-    if(l==0)
-    {
-        cout<<"\nThere are infinitely many solutions of this system of equations.\n";
-        return 0;
-    }
-    l=0;
+
+
     x=A[k][k];
     if(A[k][k]!=1)           //Row transformation: Divide all the elements of the row by its first non-zero element from left side
     {
@@ -80,7 +111,7 @@ int main()
             A[i][j]-=(A[k][j]*B[i]);
         }
     }
-    /*for(int i=0;i<R;i++)
+    for(int i=0;i<R;i++)
     {
         for(int j=0;j<C;j++)
         {
@@ -89,7 +120,8 @@ int main()
         cout<<endl;
     }
     cout<<endl;
-    */
+
+
     ++k;
     }
     while(m<(R-1))
@@ -104,19 +136,6 @@ int main()
             A[i][j]-=(A[R-m-1][j]*B[i]);
         }
     }
-    /*for(int i=0;i<R;i++)
-    {
-        for(int j=0;j<C;j++)
-        {
-            cout<<A[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<endl;
-    */
-    ++m;
-    }
-    /*cout<<"OUTPUT MATRIX\n";
     for(int i=0;i<R;i++)
     {
         for(int j=0;j<C;j++)
@@ -126,7 +145,21 @@ int main()
         cout<<endl;
     }
     cout<<endl;
-    */
+
+    ++m;
+    }
+    cout<<"OUTPUT MATRIX\n";
+    for(int i=0;i<R;i++)
+    {
+        for(int j=0;j<C;j++)
+        {
+            cout<<A[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+
     cout<<"The solution to the system of equations is\n";  //Display solutions
     for(int i=0;i<R;i++)
     {
